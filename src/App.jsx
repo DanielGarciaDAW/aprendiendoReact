d390fe4a0e237c8989263557f21f278cc2185b9e
DanielGarcia_laptop <dangargue2@alu.edu.gva.es>
2024-12-01 18:39:23 +0100

    Primer Comit
    Proyecto de React

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+import { TwitterFollowCard } from './TwitterFollowCard';
+
+
+export function App () {
+return (
+    <>
+    <TwitterFollowCard userName="daninass" name="Daniel García Guerrero"/>
+    <TwitterFollowCard userName="daninass" name="Daniel García Guerrero"/>
+    <TwitterFollowCard userName="daninass" name="Daniel García Guerrero"/>
+    </>
+)
+}
