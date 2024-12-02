f7ed015adea77648bd2bd7567ce0332c53b03eb5
Dani Daw <dangargue2@alu.edu.gva.es>
2024-12-02 20:43:34 +0100

    Botón funcional

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,17 @@ import './App.css';
 import { TwitterFollowCard } from './TwitterFollowCard';
 
 
-export function App () {
-return (
-    <>
-    <TwitterFollowCard userName="daninass" name="Daniel García Guerrero"/>
-    <TwitterFollowCard userName="Popa" name="Sofia Nieto Alonso"/>
-    <TwitterFollowCard userName="Julette" name="Julia Seva"/>
-    <TwitterFollowCard userName="Midudev" name="Miguel Angel Duran"/>
-    </>
-)
+export function App() {
+    return (
+        <section className='App'>
+            <TwitterFollowCard  userName="daninass">
+            Daniel García
+            </TwitterFollowCard>
+
+            <TwitterFollowCard  userName="Popa">
+            Sofia Nieto Alonso
+            </TwitterFollowCard>
+     
+        </section>
+    )
 }
