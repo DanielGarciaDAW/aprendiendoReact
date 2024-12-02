788494524bc46832b6b4491d32761a294cca3173
DanielGarcia_laptop <dangargue2@alu.edu.gva.es>
2024-12-02 11:47:42 +0100

    cosas

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,9 @@ export function App () {
 return (
     <>
     <TwitterFollowCard userName="daninass" name="Daniel García Guerrero"/>
-    <TwitterFollowCard userName="daninass" name="Daniel García Guerrero"/>
-    <TwitterFollowCard userName="daninass" name="Daniel García Guerrero"/>
+    <TwitterFollowCard userName="Popa" name="Sofia Nieto Alonso"/>
+    <TwitterFollowCard userName="Julette" name="Julia Seva"/>
+    <TwitterFollowCard userName="Midudev" name="Miguel Angel Duran"/>
     </>
 )
 }
