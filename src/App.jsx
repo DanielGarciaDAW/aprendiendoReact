be890509f578b384ac741848b73c039141c036e9
Dani Daw <dangargue2@alu.edu.gva.es>
2024-12-03 16:53:21 +0100

    Version Final Twitter Follow Card

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,43 @@
-import './App.css';
-import { TwitterFollowCard } from './TwitterFollowCard';
+import './App.css'
+import { TwitterFollowCard } from './TwitterFollowCard.jsx'
 
+const users = [
+  {
+    userName: 'daninass',
+    name: 'Daniel García Guerrero',
+    isFollowing: true
+  },
+  {
+    userName: 'popa',
+    name: 'Sofía Nieto Alonso',
+    isFollowing: false
+  },
+  {
+    userName: 'Patt',
+    name: 'Patricia Sánchez Mòlla',
+    isFollowing: true
+  },
+  {
+    userName: 'Arlette',
+    name: 'Arlet Mòlla García',
+    isFollowing: false
+  }
+]
 
-export function App() {
-    return (
-        <section className='App'>
-            <TwitterFollowCard  userName="daninass">
-            Daniel García
-            </TwitterFollowCard>
-
-            <TwitterFollowCard  userName="Popa">
-            Sofia Nieto Alonso
-            </TwitterFollowCard>
-     
-        </section>
-    )
+export function App () {
+  return (
+    <section className='App'>
+      {
+        users.map(({ userName, name, isFollowing }) => (
+          <TwitterFollowCard
+            key={userName}
+            userName={userName}
+            initialIsFollowing={isFollowing}
+          >
+            {name}
+          </TwitterFollowCard>
+        ))
+      }
+    </section>
+  )
 }
